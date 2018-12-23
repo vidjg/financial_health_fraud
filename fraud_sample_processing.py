cac9aac78df09060f70ed5070616f7d3be2eb69e
Martin Qian <sufe.martin.qian@gmail.com>
2018-12-23 12:47:38 +0800

    Update: financial fraud sample processing v1.0.1

--- fraud_sample_processing.py
+++ fraud_sample_processing.py
@@ -8,12 +8,16 @@
     Description:        
     Author:             Martin Qian
     Create date:        2018/12/22
-    Latest version:     v1.0.0
+    Latest version:     v1.0.1
 -----------------------------------------------------
     Change Log:
 #---------------------------------------------------#
     v1.0.0              qs          2018/12/22
-    1.
+    1. Create the File
+#---------------------------------------------------#
+    v1.0.1              qs          2018/12/22
+    1. Add year & quarter column
+    2. Add output step
 -----------------------------------------------------
 
 """
@@ -48,13 +52,34 @@ if __name__ == '__main__':
         '\s*[\d,]*\d+\.?\d*\s*.?元',
         x)))
 
-    process_df['f_account'] = process_df.content.apply(lambda x: '|'.join(re.findall(
+    process_df['f_account'] = process_df.f_fake.apply(lambda x: '|'.join(re.findall(
         '(?:(?:201\d\s*年度?)[^。]*?(?:虚[增减]|调[^查]整?[增减]?[加多少]?|[多少]计提?)'
         '|(?:虚[增减]|调[^查]整?[增减]?[加多少]?|[多少]计提?)[当本]?期?[^。]*?(?:201\d\s*年度?))[^。]*?'
         '([^。，、]*?)'  # Account
         '\s*(?:\d|,)*\d+.?\d*\s*.?元',  # Amount
         x)))
 
+    process_df['year'] = process_df.f_fake.apply(lambda x: '|'.join(set(re.findall(
+        '(?:(201\d)\s*年度?)',  # Year
+        x))))
+
+    process_df['quarter'] = process_df.f_fake.apply(lambda x: '|'.join(set(re.findall(
+        '(?:201\d\s*年度?)'  # Year
+        '.{0,3}'  # Noise
+        '(?:[一二三]季[度报]?|半年报|年报|年末)'  # Quarter
+        '(?:、(?:[一二三]季[度报]?|半年报|年报|年末))*',  # Repetitive
+        x))))
+
     # Add label
     process_df['label'] = 0
     process_df.loc[process_df.f_fake != '', 'label'] = 1
+
+    # Output the result
+    result_df = process_df.loc[process_df.label == 1, ['company_name', 'stockcode', 'sname', 'f_fake', 'f_account',
+                                                       'year', 'quarter']]
+
+    today = datetime.date.today()
+    result_df.to_csv('./class_parameter/F_bad_sample_{}_processed.csv'.format(datetime.date.strftime(today, '%y%m%d')),
+                     encoding='utf-8', index=False)
+
+    print('Done!')
